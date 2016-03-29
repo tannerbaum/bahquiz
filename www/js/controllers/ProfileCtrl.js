36b77b1cee22526be48a5445e0a73873d051b5be
Richard Hoisington <Richard Hoisington>
2016-03-29 00:35:41 -0400

    profile foundation set

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,3 +1,26 @@
 app.controller('ProfileCtrl', ['$scope', function($scope) {
 
+    $scope.register = function(){
+        
+    }
+    
+    $scope.authenticate = function(){
+        $scope.signedIn = true;
+        updateBar();
+    }
+
+    function updateBar() {
+        var elem = document.getElementById("progressBar"); 
+        var width = 0;
+        var interval = setInterval(frame, 10);
+        function frame() {
+            if (width >= 76) { // this will be user progress number
+                clearInterval(interval);
+            } else {
+                width++; 
+                elem.style.width = width + '%'; 
+                document.getElementById("label").innerHTML = width * 1 + '%';
+            }
+        }
+}
 }])
