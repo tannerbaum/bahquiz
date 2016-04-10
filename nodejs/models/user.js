65f94ca32a608f0ff25ab146e2474555006d91fd
Hnoonan <hhnoonan@gmail.com>
2016-04-10 13:33:11 -0400

    totalscore
    totalscore added to user model and table

--- nodejs/models/user.js
+++ nodejs/models/user.js
@@ -3,7 +3,8 @@
 module.exports = function(sequelize, datatypes) {
 	var user = sequelize.define("user",{
 	name : datatypes.STRING,
-	pass : datatypes.STRING
+	pass : datatypes.STRING,
+	totalscore : datatypes.INTEGER
 }, {
         classMethods: {
             associate: function(models) {
